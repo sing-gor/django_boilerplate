b90e54960d39890852b791516cda3c82fd9dc4e5
星 <yusing-wong@ooutlook.com>
2020-12-19 17:31:39 +0800

    Django Boilerplate For SSR

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """docstring for ModelTests."""
+
+    def test_create_user_with_email_successful(self):
+        """test creating a new user with an email if successful """
+        email = 'test@wongyusing.com'
+        password = 'wongyusing'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_email_normailzed(self):
+        """Test the email for a new is normailzed"""
+
+        email = 'test@WONGYUSING.COM'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='wongyusing'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_mail(self):
+        """Test creating user with no email rails error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                None,
+                password='wongyusing'
+            )
+
+    def test_create_super_user(self):
+        """Test creating an super user"""
+        user = get_user_model().objects.create_superuser(
+            email="admin@wongyusing.com",
+            password='wongyusing'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
